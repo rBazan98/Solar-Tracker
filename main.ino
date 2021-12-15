22f5e481c3a22ef929800cbec2fd165ae28c5928
rBazanUCAM <94762678+rBazanUCAM@users.noreply.github.com>
2021-12-15 11:11:27 +0100

    main hecho en clase

---
+++ main.ino
@@ -0,0 +1,99 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+
+//con 10k corriente esperada ==> 15.4 mA
+ 
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_INA219 ina219(0x41);
+
+unsigned long previousMillis = 0;
+unsigned long interval = 100;
+const int chipSelect = 10;
+float shuntvoltage = 0;
+float busvoltage = 0;
+float current_mA = 0;
+float loadvoltage = 0;
+float energy = 0;
+
+
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  ina219.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    
+    ina219values();
+    displaydata();
+
+//    Serial.print("================");
+//    Serial.print("\n");
+
+    Serial.print(shuntvoltage);
+    Serial.print("\n");
+
+    Serial.print(busvoltage);
+    Serial.print("\n");
+//
+//    Serial.print(current_mA);
+//    Serial.print("\n");
+//
+//    Serial.print(loadvoltage);
+//    Serial.print("\n");
+//
+//    Serial.print(energy);
+//    Serial.print("\n");
+//
+//    Serial.print("================");
+//    Serial.print("\n");
+
+//
+//  shuntvoltage = ina219.getShuntVoltage_mV();
+//  busvoltage = ina219.getBusVoltage_V();
+//  current_mA = ina219.getCurrent_mA();
+//  loadvoltage = busvoltage + (shuntvoltage / 1000);
+//  energy = energy + loadvoltage * current_mA / 3600;
+    
+
+  }
+}
+
+void displaydata() {
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.println(loadvoltage);
+  display.setCursor(35, 0);
+  display.println("V");
+  display.setCursor(50, 0);
+  display.println(current_mA);
+  display.setCursor(95, 0);
+  display.println("mA");
+  display.setCursor(0, 10);
+  display.println(loadvoltage * current_mA);
+  display.setCursor(65, 10);
+  display.println("mW");
+  display.setCursor(0, 20);
+  display.println(energy);
+  display.setCursor(65, 20);
+  display.println("mWh");
+  display.display();
+}
+
+void ina219values() {
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  energy = energy + loadvoltage * current_mA / 3600;
+}
