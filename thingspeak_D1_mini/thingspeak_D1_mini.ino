6c779267f55f23741b7aa4b32c9e11c37a1aa279
rBazan98 <ronaldbazanes@gmail.com>
2021-12-20 07:52:32 +0100

    Falta ordenar datos en pantalla

---
+++ thingspeak_D1_mini/thingspeak_D1_mini.ino
@@ -0,0 +1,17 @@
+// Placa D1 mini (ESP8266MOD)
+
+void setup() {
+
+// Tres Variables a enviar
+  float variable_1;
+  float variable_2;
+  float variable_3;
+
+// Construir codigo a partir de aca:
+
+  
+}
+
+void loop() {
+
+}
